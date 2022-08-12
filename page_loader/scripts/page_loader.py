1882851e567c9f35134119d5633f99b1d90e0005
SevaErsh <erschidvs@yandex.ru>
2022-08-13 00:43:35 +0300

    step 2 but linter is angry

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -2,24 +2,80 @@
 import re
 import requests
 import argparse
-from os import getcwd
+from os import getcwd, mkdir
+from os.path import splitext
+from bs4 import BeautifulSoup
 
 
 def name(url: str):
     schema_idx = url.find('://')
     url = url[schema_idx + 3:]
+    path, ext = splitext(url)
+    if ext == '.html':
+        url = path
     url = re.split(r'\W+', url)
-    url = '-'.join(url) + '.html'
+    url = '-'.join(url)
     return url
+
+
+def name_pic(url: str, src: str, directory: str):
+    path, _ = splitext(src)
+    file_name = re.split(r'\W+', path[1:])
+    file_name = '-'.join(file_name) + '.png'
+    file_name = name(url) + '-' + file_name
+    return directory + '/' + file_name
+
+
+def download_pics(url: str, src: str, directory: str, file):
+    if url[-1] == '/':
+        url = url[:len(url) - 1]
+    img = requests.get(url + src)
+    path_storage = name_pic(url, src, directory)
+    img_storage = open(path_storage, 'wb')
+    img_storage.write(img.content)
+    img_storage.close()
+    return path_storage
+
+
+def edit_html(path, url, directory):
+    file = open(path)
+    soup = BeautifulSoup(file, 'html.parser')
+    images = soup.find_all('img')
+    for image in images:
+        new_src = name_pic(url, image.get('src'), directory)
+        image['src'] = new_src
+    to_write = soup.prettify()
+    file.close()
+    file = open(path, 'w')
+    file.write(to_write)
+
+
+def find_src(file):
+    soup = BeautifulSoup(file, features="html.parser")
+    images = soup.find_all('img')
+    if images == []:
+        return None
+    return [img.get('src') for img in images if img.get('src')[0] == '/'] #костыли наше всё
 
 
 def download(url: str, dirname=None):
     if dirname is None or dirname == 'current':
         dirname = getcwd()
-    path = dirname + '/' + name(url)
+    path = dirname + '/' + name(url) + '.html'
     info = requests.get(url).text
-    file = open(path, "w")
+    file = open(path, "w+")
     file.write(info)
+    file.close()
+    file = open(path)
+    images = find_src(file)
+    if images is None:
+        return path
+    dir_files = name(url) + '_files'
+    directory = dirname + '/' + dir_files
+    mkdir(directory)
+    for image in images:
+        download_pics(url, image, directory, file)
+    edit_html(path, url, dir_files)
     file.close()
     return path
 
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,23 +1,46 @@
 import tempfile
 import page_loader as p
+from bs4 import BeautifulSoup
+from os.path import exists, join
 
 
 def test_name():
     assert p.name('https://ru.hexlet.io/courses') == \
-        'ru-hexlet-io-courses.html'
-    assert p.name('https://vk.com/username') == 'vk-com-username.html'
+        'ru-hexlet-io-courses'
+    assert p.name('https://vk.com/username') == 'vk-com-username'
+    assert p.name('https://anything.com/pic.html') == 'anything-com-pic'
+    assert p.name('http://test.com') == 'test-com'
 
 
-def test_download_path():
+def test_download_path(requests_mock):
+    requests_mock.get('https://ru.hexlet.io/courses', text='data')
     with tempfile.TemporaryDirectory() as tmpdirname:
         path = p.download('https://ru.hexlet.io/courses', tmpdirname)
         assert path == tmpdirname + '/ru-hexlet-io-courses.html'
 
 
 def test_download_file(requests_mock):
-    requests_mock.get('http://test.com', text='data')
+    requests_mock.get('http://test.com', text=open('tests/fixtures/expected.html').read())
     with tempfile.TemporaryDirectory() as tmpdirname:
         p.download('http://test.com', tmpdirname)
         expected = open('tests/fixtures/expected.html').read()
         current = open(tmpdirname + '/test-com.html').read()
         assert expected == current
+
+
+def test_download_pics(requests_mock):
+    requests_mock.get('http://test.com/picture', text=open('tests/fixtures/pic.html').read())
+    requests_mock.get('http://test.com/picture/some/beautiful/pic.png', body=open('tests/fixtures/some/beautiful/pic.png', 'rb'))
+    requests_mock.get('http://test.com/picture/any/pics.jpeg', body=open('tests/fixtures/pics.jpg', 'rb'))
+    expected_pic = open('tests/fixtures/expected_pic.html', 'r').read()
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        p.download('http://test.com/picture', tmpdirname)
+        current = open(tmpdirname + '/test-com-picture.html').read()
+        new_dir = tmpdirname + "/test-com-picture_files"
+        new_file = new_dir + "/test-com-picture-some-beautiful-pic.png"
+        new_file1 = new_dir + "/test-com-picture-any-pics.png"
+        assert exists(new_dir)
+        assert exists(new_file)
+        assert exists(new_file1)
+        assert expected_pic == current
+        #добавил в фикстуры путь до картинки, но не уверен, что сработает как надо (смотри pic.html)
