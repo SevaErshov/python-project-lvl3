6c5da4549f498993f3198435199bb905eb35984d
SevaErsh <erschidvs@yandex.ru>
2022-08-19 21:28:35 +0300

    step 4?

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -4,6 +4,14 @@ import argparse
 from os import getcwd
 from page_loader.working_with_files import find_src, download_files
 from page_loader.naming.give_name import name
+import logging
+from os.path import exists
+
+
+logging.basicConfig(level='DEBUG')
+logger = logging.getLogger()
+
+logging.getLogger('urllib3').setLevel('CRITICAL')
 
 
 def create_html_page(url, dirname):
@@ -16,8 +24,10 @@ def create_html_page(url, dirname):
 
 
 def download(url: str, dirname=None):
+    logger.info(f'requested url: {url}')
     if dirname is None or dirname == 'current':
         dirname = getcwd()
+    logger.info(f'output path: {dirname}')
     path = create_html_page(url, dirname)
     file = open(path)
     files = find_src(file, url)
@@ -25,6 +35,7 @@ def download(url: str, dirname=None):
     if files is None or files == []:
         return path
     download_files(url, dirname, path, files)
+    logger.info(f'Page was downloaded as \'{dirname}\'')
     return path
 
 
